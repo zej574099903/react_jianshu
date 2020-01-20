6c43c38515cef75088788c395ae01d91c00b30af
zej <zej15158802043>
2020-01-20 20:18:31 +0800

    首页布局

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+// 引入Topic,List,Recommend,Writer组件
+import Topic from "./components/Topic";
+import List from "./components/List";
+import Recommend from "./components/Recommend";
+import Writer from "./components/Writer";
 // 引入首页样式
 import { HomeWrapper, HomeLeft, HomeRight } from "./style";
 
@@ -12,8 +17,13 @@ class Home extends Component {
             src={require("../../statics/imgs/productTeam.jpg")}
             alt="图片无法显示"
           />
+          <Topic></Topic>
+          <List></List>
         </HomeLeft>
-        <HomeRight>right</HomeRight>
+        <HomeRight>
+          <Recommend></Recommend>
+          <Writer></Writer>
+        </HomeRight>
       </HomeWrapper>
     );
   }
